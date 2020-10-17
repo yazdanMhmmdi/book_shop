20aad8637fddbe47db3279a651440e541560092b
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2020-10-16 17:31:47 -0700

    add some basic ui component
    add some basic ui component without any styling and add divider color to specified file.

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -7,4 +7,5 @@ class IColors {
   static final Color green = Color(0xffA2DE96);
   static final Color boldGreen = Color(0xff3CA59E);
   static final Color lowedBoldGreen = Color(0xff3CA59E).withOpacity(0.15); 
+  static final Color grey = Color(0xffF4F4F4);
 }
--- lib/ui/details_screen.dart
+++ lib/ui/details_screen.dart
@@ -8,20 +8,177 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: IColors.green,
       body: SafeArea(
-          child: Column(
+          child: Stack(
         children: [
-          SizedBox(height: 135),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(38),
-                      topRight: Radius.circular(38),
-                      bottomLeft: Radius.zero,
-                      bottomRight: Radius.zero)),
+          Column(
+            children: [
+              SizedBox(height: 135),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 112),
+                    child: Directionality(
+                      textDirection: TextDirection.rtl,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Center(
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'جیسون پیترسون',
+                                    style: TextStyle(
+                                        fontFamily: "iranSans",
+                                        fontSize: 20,
+                                        color: Colors.black38),
+                                  ),
+                                  Text(
+                                    'قتل های انفجاری',
+                                    style: TextStyle(
+                                        fontFamily: "iranSans",
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.black87),
+                                  ),
+                                  SizedBox(height: 10,),
+                                  Divider(
+                                    height: 20,
+                                    color: IColors.grey,
+                                  )
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 8,),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text('درباره کتاب'),
+                                    SizedBox(height: 4,),
+                                    Container(
+                                      width: 58,
+                                      height: 3,
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(3),
+                                        color: IColors.boldGreen,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                Text('اطلاعات ناشر'),
+
+                              ],
+                            ),
+                            SizedBox(height: 16,),
+                            Container(
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: IColors.boldGreen.withOpacity(0.15),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric( vertical: 20),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text('زبان'),
+                                        Text('فارسی')
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text('زبان'),
+                                        Text('فارسی')
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text('زبان'),
+                                        Text('فارسی')
+                                      ],
+                                    ),
+                                    Column(
+                                      children: [
+                                        Text('زبان'),
+                                        Text('فارسی')
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 16,),
+                            Text('درباره کتاب'),
+                            SizedBox(height: 8,),
+                            Text('لورم لورم ایپسوم فارسی'),
+                            SizedBox(height: 8,),
+                            Container(
+                              width: double.infinity,
+                              height: 46,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: IColors.boldGreen,
+                              ),
+                              child: Center(child: Text('خرید')),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(38),
+                          topRight: Radius.circular(38),
+                          bottomLeft: Radius.zero,
+                          bottomRight: Radius.zero)),
+                ),
+              )
+            ],
+          ),
+          Container(
+            width: double.infinity,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 35,
+                ),
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: 87,
+                      height: 200,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue[200],
+                          boxShadow: [
+                            BoxShadow(
+                                offset: Offset(0, 12),
+                                blurRadius: 22,
+                                color: Colors.black.withOpacity(0.25))
+                          ]),
+                    ),
+                    Container(
+                      width: 136,
+                      height: 200,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.redAccent),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          )
+          ),
         ],
       )),
     );

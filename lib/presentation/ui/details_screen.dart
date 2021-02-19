223974e66a66f5c45fc98a26ea6a1a3aaf1d1541
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-02-19 13:36:11 +0330

    fix slider object duration & set it as a unique widget

--- lib/presentation/ui/details_screen.dart
+++ lib/presentation/ui/details_screen.dart
@@ -12,8 +12,8 @@ import 'package:book_shop/presentation/animation/pull_up_transition.dart';
 import 'package:book_shop/presentation/widgets/my_button.dart';
 import 'package:book_shop/presentation/widgets/no_network_flare.dart';
 import 'package:book_shop/presentation/widgets/progress_button.dart';
+import 'package:book_shop/presentation/widgets/slider_object.dart';
 import 'package:book_shop/presentation/widgets/widgets.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -42,12 +42,7 @@ class _DetailsScreenState extends State<DetailsScreen>
   bool _pullUpAnimationFlag = true;
   bool _fadeAnimationFlag = true;
   AnimationController _pullUpController;
-  // slider Animation
-  Color _sliderLeftTextColor = Colors.black38;
-  Color _sliderRightTextColor = Colors.black87;
-  int _sliderTextDuration = 300;
-  AlignmentGeometry _sliderObjectAlignment = Alignment.centerRight;
-  CarouselController _pageCarouselController = CarouselController();
+
   Random _random = new Random();
   int _randAge, _randBookCount, _randCategory, _randVote;
 
@@ -56,10 +51,6 @@ class _DetailsScreenState extends State<DetailsScreen>
 
   @override
   void initState() {
-    _randAge = _random.nextInt(90);
-    _randBookCount = _random.nextInt(20);
-    _randCategory = _random.nextInt(5);
-    _randVote = _random.nextInt(5);
     _pullUpController = new AnimationController(
         vsync: this, duration: Duration(milliseconds: 300));
 
@@ -168,119 +159,11 @@ class _DetailsScreenState extends State<DetailsScreen>
                                       SizedBox(
                                         height: 8,
                                       ),
-                                      Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Column(
-                                            children: [
-                                              GestureDetector(
-                                                onTap: () {
-                                                  sliderAnimationRightClicked();
-                                                },
-                                                child: AnimatedDefaultTextStyle(
-                                                  duration: Duration(
-                                                      milliseconds:
-                                                          _sliderTextDuration),
-                                                  child: Text(
-                                                    Strings.detailAboutBook,
-                                                  ),
-                                                  style: TextStyle(
-                                                      fontFamily: "iranSans",
-                                                      fontSize: 20,
-                                                      fontWeight:
-                                                          FontWeight.w700,
-                                                      color:
-                                                          _sliderRightTextColor),
-                                                ),
-                                              ),
-                                              SizedBox(
-                                                height: 4,
-                                              ),
-                                            ],
-                                          ),
-                                          GestureDetector(
-                                            onTap: () {
-                                              sliderAnimationLeftClicked();
-                                            },
-                                            child: AnimatedDefaultTextStyle(
-                                              duration: Duration(
-                                                  milliseconds:
-                                                      _sliderTextDuration),
-                                              child: Text(
-                                                Strings.detailABoutPublisher,
-                                              ),
-                                              style: TextStyle(
-                                                  fontFamily: "iranSans",
-                                                  fontSize: 20,
-                                                  fontWeight: FontWeight.w700,
-                                                  color: _sliderLeftTextColor),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      Stack(
-                                        children: [
-                                          AnimatedAlign(
-                                            duration: Duration(
-                                                milliseconds:
-                                                    _sliderTextDuration),
-                                            alignment: _sliderObjectAlignment,
-                                            child: Padding(
-                                              padding:
-                                                  const EdgeInsets.symmetric(
-                                                      horizontal: 10),
-                                              child: Container(
-                                                width: 58,
-                                                height: 3,
-                                                decoration: BoxDecoration(
-                                                  borderRadius:
-                                                      BorderRadius.circular(3),
-                                                  color: IColors.boldGreen,
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      SizedBox(
-                                        height: 16,
-                                      ),
-                                      AbsorbPointer(
-                                        child: CarouselSlider(
-                                          carouselController:
-                                              _pageCarouselController,
-                                          options: CarouselOptions(
-                                              onPageChanged: null,
-                                              disableCenter: true,
-                                              scrollDirection: Axis.horizontal,
-                                              autoPlay: false,
-                                              height: 88,
-                                              viewportFraction: 1,
-                                              initialPage: 1,
-                                              enableInfiniteScroll: false),
-                                          items: [
-                                            objectSliderItem(
-                                                "سن",
-                                                _randAge.toString(),
-                                                "تعداد کتاب",
-                                                (_randBookCount.toString() +
-                                                    " جلد "),
-                                                "دسته بندی",
-                                                _randCategory.toString(),
-                                                "مجموع آرا",
-                                                (_randVote.toString())),
-                                            objectSliderItem(
-                                                "زبان",
-                                                language,
-                                                "جلد",
-                                                coverType,
-                                                "صفحه",
-                                                pagesCount,
-                                                "رای",
-                                                voteCount),
-                                          ],
-                                        ),
+                                      SliderObject(
+                                        coverType: coverType,
+                                        voteCount: voteCount,
+                                        pageCount: pagesCount,
+                                        language: language,
                                       ),
                                       SizedBox(
                                         height: 16,
@@ -396,27 +279,6 @@ class _DetailsScreenState extends State<DetailsScreen>
     );
   }
 
-  Widget littleDialogBox(String title, String subTitle) {
-    return Column(
-      children: [
-        Text(
-          '$title',
-          style: TextStyle(
-            fontFamily: "iranSans",
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
-            color: Colors.black87,
-          ),
-        ),
-        Text(
-          '$subTitle',
-          style: TextStyle(
-              fontFamily: "iranSans", fontSize: 18, color: Colors.black38),
-        )
-      ],
-    );
-  }
-
   Widget myButton(Function onTap) {
     return Container(
       width: double.infinity,
@@ -462,61 +324,6 @@ class _DetailsScreenState extends State<DetailsScreen>
     _pullUpController.repeat(reverse: true);
     Timer(Duration(milliseconds: _animationDuration), () {
       _pullUpController.stop();
-    });
-  }
-
-  void sliderAnimationLeftClicked() {
-    setState(() {
-      _sliderLeftTextColor = Colors.black87;
-      _sliderRightTextColor = Colors.black38;
-      _sliderObjectAlignment = Alignment.centerLeft;
-      _pageCarouselController.previousPage(
-          duration: Duration(milliseconds: _sliderTextDuration));
-    });
-  }
-
-  void sliderAnimationRightClicked() {
-    setState(() {
-      _sliderLeftTextColor = Colors.black38;
-      _sliderRightTextColor = Colors.black87;
-      _sliderObjectAlignment = Alignment.centerRight;
-      _pageCarouselController.nextPage(
-          duration: Duration(milliseconds: _sliderTextDuration));
-    });
-  }
-
-  Widget objectSliderItem(
-      String first,
-      String second,
-      String third,
-      String fourth,
-      String fifth,
-      String sixth,
-      String seventh,
-      String eighth) {
-    return Builder(builder: (BuildContext context) {
-      return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 4),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: IColors.boldGreen.withOpacity(0.15),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                littleDialogBox("${second}", "$first"),
-                littleDialogBox("${fourth}", "$third"),
-                littleDialogBox("${sixth}", "$fifth"),
-                littleDialogBox("${eighth}", "$seventh")
-              ],
-            ),
-          ),
-        ),
-      );
     });
   }
 }

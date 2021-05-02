d701a9833d2da3cc4f83db80ae7276911bc09f1e
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-05-02 21:44:23 +0430

    create from bubble message ui

---
+++ lib/presentation/widgets/from_message_bubble.dart
@@ -0,0 +1,38 @@
+import 'package:book_shop/constants/colors.dart';
+import 'package:book_shop/constants/strings.dart';
+import 'package:flutter/material.dart';
+
+class FromMessageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 60, top: 8),
+      child: Flex(
+        direction: Axis.vertical,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: IColors.balck15,
+                    borderRadius: BorderRadius.circular(15)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Strings.detailsLoremIpsom",
+                    style: TextStyle(
+                      fontFamily: Strings.fontIranSans,
+                      color: IColors.balck85,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}

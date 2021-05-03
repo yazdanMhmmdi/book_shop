13887103cad8bdb02a14278611825aa72f7f0256
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-05-03 17:19:04 +0430

    fix typo

--- lib/data/repository/chat_repository.dart
+++ lib/data/repository/chat_repository.dart
@@ -2,13 +2,12 @@ import 'package:book_shop/data/model/chat_list_model.dart';
 import 'package:book_shop/data/model/details_model.dart';
 import 'package:book_shop/networking/api_provider.dart';
 
-class ChatMessagesRepository {
+class ChatRepository {
   ApiProvider _apiProvider = new ApiProvider();
 
-  Future<ChatListModel> getChatMessages(
-      String user_id, String book_id, String page) async {
-    final response = await _apiProvider.get(
-        '/chat_messages_api.php?user_id=${user_id}&page=${page}&book_id=${book_id}');
+  Future<ChatListModel> getChatMessages(String user_id,String book_id, String page) async {
+    final response = await _apiProvider
+        .get('/chat_messages_api.php?user_id=${user_id}&page=${page}&book_id=${book_id}');
     return ChatListModel.fromJson(response);
   }
 }

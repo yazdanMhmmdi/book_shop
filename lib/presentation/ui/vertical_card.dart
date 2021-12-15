2f1a2a8e1595f2b4e80ba0e6008a898eb7c31cfb
Yazdan Mohammadi <yazdanmohammadi00@gmail.com>
2021-12-15 18:13:08 +0330

    add image cacher & animation

--- lib/presentation/ui/vertical_card.dart
+++ lib/presentation/ui/vertical_card.dart
@@ -7,6 +7,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:intl/intl.dart';
+import 'package:octo_image/octo_image.dart';
 
 class VerticalCard extends StatelessWidget {
   String image;
@@ -88,10 +89,19 @@ class VerticalCard extends StatelessWidget {
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(8),
                             color: Colors.transparent,
-                            image: DecorationImage(
-                                image: CachedNetworkImageProvider(
-                                    ImageAddressProvider.imageURL +
-                                        thumbImage)),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: OctoImage(
+                              image: CachedNetworkImageProvider(
+                                ImageAddressProvider.imageURL + thumbImage,
+                              ),
+                              placeholderBuilder: OctoPlaceholder.blurHash(
+                                'LEHV6nWB2yk8pyo0adR*.7kCMdnj',
+                              ),
+                              errorBuilder: OctoError.icon(color: Colors.red),
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
                       ),
